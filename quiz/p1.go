23b4a44b75f5a5bd88e980a9e87a8548e3265da2
Shashwat Pathak <shashwatpathak100@gmail.com>
2025-09-15 05:23:17 +0530

    finished part1/2 quiz

---
+++ quiz/p1.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	csv_reader := csv.NewReader(f)
+	csv_data, err := csv_reader.ReadAll()
+	correct := 0
+	total := len(csv_data)
+	var user_input string
+	if err == io.EOF {
+		return
+	}
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	timer := time.NewTimer(15 * time.Second)
+	answerCh := make(chan string)
+quizloop:
+	for _, row_value := range csv_data {
+		// fmt.Printf("%+v\n", l)
+
+		fmt.Print(row_value[0], ": ")
+		go func() {
+			fmt.Scanln(&user_input)
+			answerCh <- user_input
+		}()
+
+		select {
+		case <-timer.C:
+			break quizloop
+		case user_input := <-answerCh:
+			if user_input == row_value[1] {
+				correct++
+			}
+		}
+
+	}
+	fmt.Printf("score: %d/%d\n", correct, total)
+}
